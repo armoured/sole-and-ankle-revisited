4bad2c7202aff979bacd5519db1da9cef2b21b6c
Mitchell <mitchell_shelton@y7mail.com>
2022-02-21 19:20:02 +1100

    impr: Exercise 1 setup breakpoints

--- src/constants.js
+++ src/constants.js
@@ -16,3 +16,15 @@ export const WEIGHTS = {
   medium: 600,
   bold: 800,
 };
+
+const BREAKPOINTS = {
+  laptopMax: 1300/16,
+  tabletMax: 950/16,
+  phoneMax: 600/16
+};
+
+export const QUERIES = {
+  'laptopAndDown': `(max-width: ${BREAKPOINTS.laptopMax}rem)`,
+  'tabletAndDown': `(max-width: ${BREAKPOINTS.tabletMax}rem)`,
+  'phoneAndDown': `(max-width: ${BREAKPOINTS.phoneMax}rem)`
+}
